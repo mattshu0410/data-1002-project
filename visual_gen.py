4bd12b6d915cd232910d4c91ede8f73216e6a20c
Matthew Shu <mshu9220@uni.sydney.edu.au>
2020-11-03 15:31:55 +1100

    Visuals Generation

--- visual_gen.py
+++ visual_gen.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import plotly.express as px
 sns.set_theme()
 
 #import cleaned data from repository
@@ -8,8 +9,8 @@ df = pd.read_csv("https://raw.githubusercontent.com/mattshu0410/data-1002-projec
 
 #Plot 1: Does the GNI per capita of different countries display positive correlation with improved human freedom and happiness scores?
 #select indicators as separate dataframes
-Freedom = df[['hf_score','GNI_capita']]
-Happiness = df[['Happiness.Score', 'GNI_capita']]
+Freedom = df[['countries','hf_score','GNI_capita']]
+Happiness = df[['countries','Happiness.Score', 'GNI_capita']]
 #Correlation of Linear Model & Residual Distribution
 print(Freedom["hf_score"].corr(Freedom["GNI_capita"]))
 print(Happiness["Happiness.Score"].corr(Happiness["GNI_capita"]))
@@ -36,15 +37,29 @@ plt.legend(labels=("HFI Regression Line", "Happiness Regression Line"))
 plt.savefig("fig1_hap_freedom_regplot", bbox_inches='tight', dpi=500)
 plt.show()
 
-#Plot 2: What is the relationship between different regions and their respective happiness and freedom?
+#Plot 2: Interactive Plot with Four Variables
+print(combined_indicators)
+fig = px.scatter(combined_indicators, x='x', y='y', color='Indicator', text = 'countries', trendline='ols',
+                 labels={
+                     'x':'GNI per Capita by PPP by Purchasing Power Parity (2017 International $)',
+                     'y':'Standard Indicator Score (0-10)',
+                     'countries':'Country'
+                 },
+                 title = 'HFI & Happiness Regressed on GNI/capita'
+                 )
+fig.layout.template = 'plotly_dark'
+fig.write_html("M:/Github Version Control Projects/data-1002-project/fig2_hap_freedom_regplot_interactive.html")
+fig.show()
+
+#Plot 3: What is the relationship between different regions and their respective happiness and freedom?
 #select indicators data with region to identify clustering
 indicator_by_region = df[['region', 'hf_score', 'Happiness.Score']]
 h = sns.jointplot(data=indicator_by_region, x='hf_score', y='Happiness.Score', hue='region', height=10)
 h.set_axis_labels('Human Freedom Score (0-10)', 'Happiness Score (0-10)')
-plt.savefig("fig2_hap_freedom_region", dpi=500)
+plt.savefig("fig3_hap_freedom_region", dpi=500)
 plt.show()
 
-
+#Plot 4: Barplot of means of happiness and freedom by region
 means = indicator_by_region.groupby("region").mean()
 means = means.reset_index()
 #https://stackoverflow.com/questions/36537945/reshape-wide-to-long-in-pandas
@@ -59,5 +74,5 @@ for item in i.get_xticklabels():
 plt.xlabel("World Region")
 plt.ylabel("Mean Score (0-10)")
 plt.tight_layout()
-plt.savefig("fig3_hap_free_means_by_region", dpi=500)
+plt.savefig("fig4_hap_free_means_by_region", dpi=500)
 plt.show()
