eee936f72303fcd62d11ca6da6dc0c8c78abeb25
Matthew Shu <mshu9220@uni.sydney.edu.au>
2020-11-06 10:57:35 +1100

    Visuals Generation

---
+++ Project Stage 1 Submission/README.txt
@@ -0,0 +1,14 @@
+1) The final merged data set was constructed with data from the following sources. The details of each source
+and its licences/restrictions can be found in the Project Stage 1 Report | Data Sources Section.
+https://www.kaggle.com/mathurinache/world-happiness-report?select=2020.csv
+https://www.kaggle.com/gsutters/the-human-freedom-index?select=hfi_cc_2019.csv
+https://data.worldbank.org/indicator/NY.GNP.PCAP.PP.KD
+https://www.kaggle.com/juanumusic/countries-iso-codes
+
+
+2) Merging on 2017 data and country codes, renaming of columns, removing missing values, checking for empty
+values and removing associated rows, checking for duplicate rows, checking for expect value ranges. Beginning
+with 246 entries we ended with 133 entries and 9 variables. More details on the exact cleaning process with 
+code can be found in Project Stage 1 Report | Summary of our Cleaning Section.
+
+3) Please see the contained Metadata PDF for a data dictionary.
--- visual_gen.py
+++ visual_gen.py
@@ -1,7 +1,10 @@
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
 sns.set_theme()
 
 #import cleaned data from repository
@@ -26,15 +29,72 @@ combined_indicators = pd.concat([Freedom.rename(columns={'GNI_capita':'x','hf_sc
                 Happiness.rename(columns={'GNI_capita':'x','Happiness.Score':'y'})
                 .join(pd.Series(['Happiness']*len(Happiness), name='Indicator'))],
                ignore_index=True)
-g = sns.FacetGrid(combined_indicators, hue='Indicator', height=5)
-g.map(plt.scatter, "x", "y", s=50, alpha=.7, linewidth=.5, edgecolor="white")
-g.map(sns.regplot, "x", "y", ci=None, robust=1)
-g.add_legend()
+
+combined_indicators["GNI_capita_bins"] = pd.cut(combined_indicators["x"], bins=[0,1025,4035,12475,float('inf')]) #These values from World Bank, Max is Arbitrary
+print(combined_indicators)
+
+#EXPERIMENT
+
+sns.lmplot(data=combined_indicators, x='x', y='y', hue='Indicator', palette=["#000620"], markers=['x', '.'], ci=None)
+#sns.scatterplot(data=combined_indicators, x="x", y="y",style='Indicator', s=50, alpha=.7, linewidth=.5, edgecolor="white", markers=True)
+
+plt.axvspan(0,1025, alpha=0.5, color='#de425b', zorder=-1)
+plt.axvspan(1025,4035, alpha=0.5, color='#fbb862', zorder=-1)
+plt.axvspan(4035,12475, alpha=0.5, color ='#afd17c', zorder=-1)
+plt.axvspan(12475,100000, alpha=0.5, color='#00876c', zorder=-1)
 plt.title("HFI & Happiness Regressed on GNI/capita")
 plt.xlabel("GNI/Capita by Purchasing Power Parity (2017 International $)")
 plt.ylabel("Standard Indicator Score (0-10)")
 plt.legend(labels=("HFI Regression Line", "Happiness Regression Line"))
-plt.savefig("fig1_hap_freedom_regplot", bbox_inches='tight', dpi=500)
+plt.savefig("Figures/fig1_hap_freedom_regplot.png", bbox_inches='tight', dpi=500)
+plt.show()
+#EXPERIMENT
+
+#EXPERIMENT
+x_col = 'x'
+y_col = 'y'
+hue_col = 'Indicator'
+df = combined_indicators
+
+markers = ['x','.']
+colors = ["#000620", "#000620"]
+linestyles = ['-','--']
+
+legend_elements = [Line2D([0], [0], color='#000620', lw=4, label='Freedom', linestyles='--'),
+                   Line2D([0],[0], color='#000620', lw=4, label='Happiness'),
+                   Line2D([0], [0], marker='x', color='#000620', label='Freedom',
+                          markerfacecolor='#000620', markersize=10),
+                   Line2D([0], [0], marker='.', color='#000620', label='Happiness',
+                          markerfacecolor='#000620', markersize=10),
+                   Patch(facecolor='#de425b', label='Color Patch')]
+
+plt.figure()
+plt.axvspan(0,1025, alpha=0.5, color='#de425b', zorder=-1)
+plt.axvspan(1025,4035, alpha=0.5, color='#fbb862', zorder=-1)
+plt.axvspan(4035,12475, alpha=0.5, color ='#afd17c', zorder=-1)
+plt.axvspan(12475,100000, alpha=0.5, color='#00876c', zorder=-1)
+
+for (hue,gr),m,c,ls in zip(df.groupby(hue_col),markers,colors,linestyles):
+    sns.regplot(data=gr, x=x_col, y=y_col, marker=m, color=c, line_kws={'ls':ls}, ci=None, label=f'{hue_col}={hue}')
+plt.title("HFI & Happiness Regressed on GNI/capita")
+plt.xlabel("GNI/Capita by Purchasing Power Parity (2017 International $)")
+plt.ylabel("Standard Indicator Score (0-10)")
+plt.legend()
+#EXPERIMENT
+
+g = sns.FacetGrid(combined_indicators, height=5, hue='Indicator')
+g.map(plt.scatter, "x", "y", s=50, alpha=.7, linewidth=.5, edgecolor="white")
+g.map(sns.regplot, "x", "y", ci=None, robust=1)
+g.add_legend()
+plt.axvspan(0,1025, alpha=0.3, color='#ffa600')
+plt.axvspan(1025,4035, alpha=0.3, color='#ff6361')
+plt.axvspan(4035,12475, alpha=0.3, color ='#bc5090')
+plt.axvspan(12475,100000, alpha=0.3, color='#58508d')
+plt.title("HFI & Happiness Regressed on GNI/capita")
+plt.xlabel("GNI/Capita by Purchasing Power Parity (2017 International $)")
+plt.ylabel("Standard Indicator Score (0-10)")
+plt.legend(labels=("HFI Regression Line", "Happiness Regression Line"))
+plt.savefig("Figures/fig1_hap_freedom_regplot.png", bbox_inches='tight', dpi=500)
 plt.show()
 
 #Plot 2: Interactive Plot with Four Variables
@@ -48,7 +108,13 @@ fig = px.scatter(combined_indicators, x='x', y='y', color='Indicator', text = 'c
                  title = 'HFI & Happiness Regressed on GNI/capita'
                  )
 fig.layout.template = 'plotly_dark'
-fig.write_html("M:/Github Version Control Projects/data-1002-project/fig2_hap_freedom_regplot_interactive.html")
+fig.write_html("M:/Github Version Control Projects/data-1002-project/Figures/fig2_hap_freedom_regplot_interactive.html")
+#import chart_studio
+#username = 'matthewshu' # your username
+#api_key = 'GZZnWMNJzOUWnhUAB7dw' # your api key - go to profile > settings > regenerate key
+#chart_studio.tools.set_credentials_file(username=username, api_key=api_key)
+#import chart_studio.plotly as py
+#py.plot(fig, filename = 'fig2_hap_freedom_regplot', auto_open=True)
 fig.show()
 
 #Plot 3: What is the relationship between different regions and their respective happiness and freedom?
@@ -56,7 +122,7 @@ fig.show()
 indicator_by_region = df[['region', 'hf_score', 'Happiness.Score']]
 h = sns.jointplot(data=indicator_by_region, x='hf_score', y='Happiness.Score', hue='region', height=10)
 h.set_axis_labels('Human Freedom Score (0-10)', 'Happiness Score (0-10)')
-plt.savefig("fig3_hap_freedom_region", dpi=500)
+plt.savefig("Figures/fig3_hap_freedom_region.png", dpi=500)
 plt.show()
 
 #Plot 4: Barplot of means of happiness and freedom by region
@@ -74,5 +140,5 @@ for item in i.get_xticklabels():
 plt.xlabel("World Region")
 plt.ylabel("Mean Score (0-10)")
 plt.tight_layout()
-plt.savefig("fig4_hap_free_means_by_region", dpi=500)
+plt.savefig("Figures/fig4_hap_free_means_by_region.png", dpi=500)
 plt.show()
